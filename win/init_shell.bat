f8a970c35123a8470bdbd14073d8858dab0a439e
David Wolfe <david.p.wolfe@gmail.com>
2015-08-30 07:52:55 -0700

    Better paths

--- win/init_shell.bat
+++ win/init_shell.bat
@@ -1,8 +1,8 @@
 @echo off
 
-doskey /MACROFILE=aliases.txt
+doskey /MACROFILE=%HOMEDRIVE%\repo\devenv\win\aliases.txt
 set PATH=%APPDATA%\npm\node_modules\grunt-cli\bin;%PATH%
 prompt $P$S$C$T$F$_$$
-cd C:\repo
+cd %HOMEDRIVE%\repo
 
 @echo on
