383da1724eb7ddb20c6618b64af6f0f8512cd725
David Wolfe <david.p.wolfe@gmail.com>
2016-01-05 08:00:53 -0800

    use 'environment' as directory

--- win/init_shell.bat
+++ win/init_shell.bat
@@ -1,6 +1,6 @@
 @echo off
 
-doskey /MACROFILE=%HOMEDRIVE%\repo\devenv\win\aliases.txt
+doskey /MACROFILE=%HOMEDRIVE%\repo\environment\win\aliases.txt
 set PATH=%APPDATA%\npm\node_modules\grunt-cli\bin;%PATH%
 prompt $P$S$C$T$F$_$$
 cd %HOMEDRIVE%\repo
