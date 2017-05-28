00e9dc62f746af9b9e314ffeeeaeeb0373c00981
David P. Wolfe <david.p.wolfe@gmail.com>
2017-05-27 17:50:43 -0700

    doc a couple update steps, -H awscli upgrade, brew brew update & upgrade

--- mac/update.sh
+++ mac/update.sh
@@ -7,9 +7,13 @@ stty echo
 printf "\n"
 
 # Install Apple software updates
+# This only works if updates were already detected by the Mac App Store
+# You will generally rely on the auto-update system feature which setup.sh enables.
 echo $PASSWORD | sudo -S -p "" softwareupdate -i -a
 
 # Install iTunes App Store updates
+# This only works if updates were already detected by the Mac App Store
+# You will generally rely on the auto-update system feature which setup.sh enables.
 mas upgrade
 
 # Update all the Atom packages
@@ -18,5 +22,7 @@ yes | apm update -c false
 # todo: Update Office
 
 # update AWS CLI
-echo $PASSWORD | sudo -S -p "" pip install --upgrade awscli
+echo $PASSWORD | sudo -H -S -p "" pip install --upgrade awscli
 
+brew update
+brew upgrade --cleanup
