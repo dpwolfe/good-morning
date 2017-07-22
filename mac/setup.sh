bc5d1a98f88983b33010a17ba4567971b72223f3
David P. Wolfe <david.p.wolfe@gmail.com>
2017-07-22 14:01:03 -0700

    fix license accepted check

--- mac/setup.sh
+++ mac/setup.sh
@@ -69,7 +69,7 @@ function dmginstall {
   fi
 }
 
-if /usr/bin/xcrun clang 2> /dev/null; then
+if ! /usr/bin/xcrun clang 2> /dev/null; then
   echo "Accepting the Xcode license..."
   sudoit xcodebuild -license accept
   echo "Installing Xcode command line tools..."
