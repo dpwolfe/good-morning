ae5023779c9f77a8c34dcc668c34e3f61f956cc1
David P. Wolfe <david@davidpwolfe.com>
2018-05-17 09:14:24 -0700

    create ssh key if there is no id_rsa

--- good-morning.sh
+++ good-morning.sh
@@ -231,7 +231,7 @@ if [ -z "$GITHUB_NAME" ]; then
 fi
 # Generate a new SSH key for GitHub https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/
 GITHUB_KEYS_URL="https://github.com/settings/keys"
-if [ -n "$gitHubEmailChanged" ] && askto "create a GitHub SSH key for $GITHUB_EMAIL"; then
+if ( [ -n "$gitHubEmailChanged" ] || ! [ -f "$HOME/.ssh/id_rsa.pub" ] ) && askto "create a GitHub SSH key for $GITHUB_EMAIL"; then
   ssh-keygen -t rsa -b 4096 -C "$GITHUB_EMAIL" < /dev/tty
   # start ssh-agent
   eval "$(ssh-agent -s)"
