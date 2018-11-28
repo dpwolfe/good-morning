b4595b947235e0eb7f42d0ea5c00d1fd5df70011
David P. Wolfe <david@davidpwolfe.com>
2018-11-27 16:45:51 -0800

    [0-0] add flags to properly build python installs with mojave

--- good-morning.sh
+++ good-morning.sh
@@ -575,12 +575,14 @@ unset brew_list_temp_file
 # prototype pyenv install code - need
 if ! pyenv versions | grep "2\.7\.15" &> /dev/null; then
   CFLAGS="-I$(brew --prefix openssl)/include" \
-  LDFLAGS="-L$(brew --prefix openssl)/lib" \
+  CPPFLAGS="-I/usr/local/opt/readline/include" \
+  LDFLAGS="-L$(brew --prefix openssl)/lib -L/usr/local/opt/readline/lib" \
   pyenv install 2.7.15
 fi
 if ! pyenv versions | grep "3\.7\.1" &> /dev/null; then
   CFLAGS="-I$(brew --prefix openssl)/include" \
-  LDFLAGS="-L$(brew --prefix openssl)/lib" \
+  CPPFLAGS="-I/usr/local/opt/readline/include" \
+  LDFLAGS="-L$(brew --prefix openssl)/lib -L/usr/local/opt/readline/lib" \
   pyenv install 3.7.1
 fi
 pyenv global 3.7.1
