3d208c05b371807f53c1a275b10e4770398848cd
David P. Wolfe <david@davidpwolfe.com>
2019-04-15 18:48:34 +0900

    update gpg keyserver for rvm install

--- good-morning.sh
+++ good-morning.sh
@@ -282,7 +282,7 @@ fi
 
 if ! [ -s "$HOME/.rvm/scripts/rvm" ] && ! type rvm &> /dev/null; then
   echo "Installing RVM..."
-  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+  gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
   curl -sSL https://get.rvm.io | bash -s stable --ruby
   # shellcheck source=/dev/null
   source "$HOME/.profile" # load rvm
