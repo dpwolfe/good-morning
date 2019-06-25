1cdc30a5af3b560356b8e1196579bb219b1d1b28
David P. Wolfe <david@davidpwolfe.com>
2019-06-25 15:59:38 -0700

    fix /Applications dir permission setting

--- good-morning.sh
+++ good-morning.sh
@@ -153,7 +153,7 @@ function checkPerms {
     /System/Library/Frameworks/Python.framework/Versions/2.7/share/doc
     /System/Library/Frameworks/Python.framework/Versions/2.7/share/man
     "$HOME/.pyenv"
-    $(ls -d /Applications)
+    $(ls -d /Applications/*.app)
   )
   local userPerm="$USER:wheel"
   for dir in "${dirs[@]}"; do
