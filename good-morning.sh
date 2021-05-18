ab14baf26e452dd609721b7037d967623dfb2c45
David Wolfe <david@davidpwolfe.com>
2021-05-18 15:47:23 -0700

    update how homebrew is installed

--- good-morning.sh
+++ good-morning.sh
@@ -578,7 +578,7 @@ function checkBrewTaps {
 
 # Install homebrew - https://brew.sh
 if ! type brew &> /dev/null; then
-  yes '' | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
   eccho "Updating Homebrew..."
   checkBrewTaps
